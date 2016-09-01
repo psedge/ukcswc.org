869568d63233004171344e3ca7c625a2cec65444
Peter Sedgewick <peter.sedgewick@gmail.com>
2016-09-01 16:06:13 +0100

    the domain propogated over night, and it's all now pointing correctly. luvly

--- backend/templatetags/session_helper.py
+++ backend/templatetags/session_helper.py
@@ -4,8 +4,9 @@ import datetime
 
 register = template.Library()
 
-
-@register.simple_tag
+# Turns out assingment tags are for objects, simple_tags are for strings.
+# No idea why this worked locally.
+@register.assignment_tag()
 def next_session(*args):
     try:
         return Date.objects.all().filter(date__gte=datetime.date.today()).order_by('date').first()
